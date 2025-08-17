b620d9080516f4fb92bd7bda88c8c2a5c5f8cd14
Mesum Abbas <i.mesumabbas#@gmail.com>
2025-08-18 02:24:16 +0500

    Fix logo size, background image opacity, and payment section issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -158,7 +158,7 @@ export default function Navbar() {
               <img 
                 src={Logo} 
                 alt="Pakistan Online Logo" 
-                className="h-16 w-auto"
+                className="h-20 w-auto"
               />
             </Link>
           </motion.div>
