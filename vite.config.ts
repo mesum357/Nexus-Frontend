4d897615fb66ded23ba103a0136c0939c0ef1531
Mesum Abbas <i.mesumabbas#@gmail.com>
2025-08-19 00:33:13 +0500

    Updated the shop city

--- vite.config.ts
+++ vite.config.ts
@@ -22,4 +22,16 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@radix-ui/react-dialog', '@radix-ui/react-select', '@radix-ui/react-popover'],
+        },
+      },
+    },
+  },
 }));
